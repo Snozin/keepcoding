cb47944627ae761f56207f12d05b3e5a13672397
Misael Duarte <msl.snzn@gmail.com>
2021-01-05 14:01:50 +0000

    Ejemplos de funciones de orden superior con funciones como parametro y retornando funciones

--- p1.funcionesOrdenSuperior.py
+++ p1.funcionesOrdenSuperior.py
@@ -1,6 +1,8 @@
-# Las funciones de orden superior son las que admiten otras funciones
-# como parametros o devuelven una funcion como resultado o ambas cosas.
+""" Las funciones de orden superior son las que admiten otras funciones
+como parametros o devuelven una funcion como resultado o ambas cosas. """
 
+
+# Ejemplos de funciones que reciben funciones como params
 def sumaNormal(n):
   return n
 
@@ -16,9 +18,47 @@ def sumaTodos(limite, funcion):
 
   return resultado
 
-print(sumaTodos(100, sumaNormal))
-print(sumaTodos(3, sumaCuadrados))
+# print(sumaTodos(100, sumaNormal))
+# print(sumaTodos(3, sumaCuadrados))
 
 """ Al llamar a la funcion sumaNormal o sumaCuadrados no se les asigna ningun
 parametro, este parametro es añadido dentro de la funcion sumaTodos cuando se
 le asigna el valor i"""
+
+
+# Ejemplos de funciones que devuelven funciones
+def minimo(*params):
+  if len(params) == 0:
+    return 0
+  
+  min = 0
+  for i in range(len(params)):
+    if params[i] < min:
+      min = params[i]
+  
+  return min
+
+def maximo(*params):
+  if len(params) == 0:
+    return 0
+  
+  max = 0
+  for i in range(len(params)):
+    if params[i] > max:
+      max = params[i]
+  
+  return max
+
+nFunciones = {
+  'max' : maximo,
+  'min' : minimo
+}
+
+def dameFuncion(nombre):
+  nombre = nombre.lower()
+  if nombre in nFunciones.keys():
+    return nFunciones[nombre]
+  
+  return None
+
+print(dameFuncion('max')(2,7,-3,1))
